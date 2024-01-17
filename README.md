ed937840aaa704a316c87c18cc12bc50e0f87941
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-17 05:07:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# S3 Data Lake Project
---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  cloud {
+    organization = "tiger_projects"
+    workspaces {
+      name = "s3-data-lake"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
